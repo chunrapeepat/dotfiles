606756e35d33ac836e69f0819107933a1610237e
chunza2542 <chunza2542@hotmail.com>
2019-07-03 16:48:55 +0700

    Add Alacritty Config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
+
 # Utilties Function
 function link_config {
    ln -s $(pwd)/$1 ~/.config
 }
-
-# Install necessary packages defined in Homebrew
-brew bundle
 
 # Link configulation files
 link_config alacritty
