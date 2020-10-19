19f1130244963a7ac38d42584586b7d022038e90
chunza2542 <chunza2542@gmail.com>
2020-10-19 10:49:05 +0700

    add install package script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# install brew package
-brew bundle --verbose
-
-# install oh my fish
-curl -L https://get.oh-my.fish | fish
---
+++ scripts/01-install-package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install brew packages
+# brew bundle --verbose
+
+echo "✅ finished; installation of packages and applications!"
